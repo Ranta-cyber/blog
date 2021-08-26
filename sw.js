5589bf5b24b11857f654170417f186a060f44f97
Sirpa Merivalssi <64065711+Ranta-cyber@users.noreply.github.com>
2021-08-26 11:56:35 +0300

    Updates

--- sw.js
+++ sw.js
@@ -26,7 +26,7 @@ workbox.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-6bd714650671abfe4fae.js"
+    "url": "webpack-runtime-fe05331d50a0223576fe.js"
   },
   {
     "url": "framework-d585a04b7d285a7760d6.js"
@@ -39,7 +39,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "47d6b394a3f296ecb66563b497507f81"
+    "revision": "597756476f110160c4aee1b06b359de7"
   },
   {
     "url": "page-data/offline-plugin-app-shell-fallback/page-data.json",
@@ -47,7 +47,7 @@ self.__precacheManifest = [
   },
   {
     "url": "page-data/app-data.json",
-    "revision": "d79eac7ee10ad03aa07360afe63234c2"
+    "revision": "e40082ab286e2c0667b7856940375fa3"
   },
   {
     "url": "polyfill-b71b9362b942c4bf9334.js"
